202f7ad72e83bbe2f6a8a925fbfae96097c50cf9
ctevnan <carolyntevnan@yahoo.com>
2016-02-07 20:12:59 -0500

    commit for a small win in getting a console.log with lots of objects

--- github.js
+++ github.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
   $.ajax({
     type: "GET",
-    url: ""
-});
+    url:"https://api.github.com/users/ctevnan/repos",
+    success: function(commits){
+      for (var i = 0; i < commits.length; i++) {
+      console.log(commits);
+    }
+  }
+}); 
+}); 
