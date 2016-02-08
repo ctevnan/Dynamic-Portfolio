174ccf481082a6b64961a54b0857096b72026972
ctevnan <carolyntevnan@yahoo.com>
2016-02-08 11:48:53 -0500

    another small success. build list, appended api data.

--- github.js
+++ github.js
@@ -2,13 +2,27 @@ $(document).ready(function() {
     $.ajax({
         type: "GET",
         url:"https://api.github.com/users/ctevnan/repos",
-        success: function(commits){
-          for (var i = 0; i < commits.length; i++) {
-            console.log(commits[i]);
+        success: function(repos) {
+          for (var i = 0; i < repos.length; i++) {
+            var newListItem = buildListGroup(repos[i]);
+            $(".list-group").append(newListItem);
           }
         },
         error: function(jqXHR, textStatus, errorThrown) {
           alert("Something went wrong. Oops.");
         }
-    }); 
+    });
+
+    function buildListGroup(repoData) {
+      var apiUrl = "https://api.github.com/repos/";
+      apiUrl += repoData.owner.login + "/";
+      apiUrl += repoData.name;
+
+      var newLink = $("<a>")
+        .attr("href", apiUrl)
+        .append(repoData.full_name)
+        .addClass("list-group-item")
+        .append(repoData.full_name);
+      return newLink;
+    } 
 }); 
