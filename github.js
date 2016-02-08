d7512efe2615fffa123612b78e871889755ad3a4
ctevnan <carolyntevnan@yahoo.com>
2016-02-07 20:33:15 -0500

    another small success. getting object, id, name, full name, owner, private: false in console

--- github.js
+++ github.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     url:"https://api.github.com/users/ctevnan/repos",
     success: function(commits){
       for (var i = 0; i < commits.length; i++) {
-      console.log(commits);
+      console.log(commits[i]);
     }
   }
 }); 
