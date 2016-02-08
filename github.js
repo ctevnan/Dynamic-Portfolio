7aff210e679542887dccaa72866f73c3c5e079d6
ctevnan <carolyntevnan@yahoo.com>
2016-02-08 10:06:10 -0500

    small success: error function added

--- github.js
+++ github.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
-  $.ajax({
-    type: "GET",
-    url:"https://api.github.com/users/ctevnan/repos",
-    success: function(commits){
-      for (var i = 0; i < commits.length; i++) {
-      console.log(commits[i]);
-    }
-  }
-}); 
+    $.ajax({
+        type: "GET",
+        url:"https://api.github.com/users/ctevnan/repos",
+        success: function(commits){
+          for (var i = 0; i < commits.length; i++) {
+            console.log(commits[i]);
+          }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          alert("Something went wrong. Oops.");
+        }
+    }); 
 }); 
